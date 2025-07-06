e7088ef4f290cd4a78b46a667e2ece19fb098861
SuperSocialForce <gyxcyh5g@gmail.com>
2025-07-06 15:55:42 +0900

    [update] vis

--- docs/script.js
+++ docs/script.js
@@ -22,8 +22,9 @@ class ThreeApp {
     height: window.innerHeight, 
   };
   static MATERIAL_PARAM = {
-    color: 0x3399ff,
+    color: 0x4080ff,
   };
+  static SCALE = 1.0 / 6378e3;
 
   renderer;   
   scene;      
@@ -52,14 +53,25 @@ class ThreeApp {
     this.camera.position.copy(ThreeApp.CAMERA_PARAM.position);
     this.camera.lookAt(ThreeApp.CAMERA_PARAM.lookAt);
 
-    this.geometry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
-    this.material = new THREE.MeshBasicMaterial(ThreeApp.MATERIAL_PARAM);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2.0);
+    directionalLight.position.set(1.0, 1.0, 1.0);
+    this.scene.add(directionalLight);
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+    this.scene.add(ambientLight);
 
-    this.box = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.box);
+    this.sphere = new THREE.SphereGeometry(1.0, 32, 32);
+    this.material = new THREE.MeshPhongMaterial(ThreeApp.MATERIAL_PARAM);
+    this.earth = new THREE.Mesh(this.sphere, this.material);
+    this.scene.add(this.earth);
 
-    const axesBarLength = 5.0;
-    this.axesHelper = new THREE.AxesHelper(axesBarLength);
+    const plane = new THREE.PlaneGeometry(3, 3);
+    this.material = new THREE.MeshBasicMaterial({ transparent: true, color: 0xb08080, opacity: 0.5, side: THREE.DoubleSide });
+    const equatorialPlane = new THREE.Mesh(plane, this.material);
+    equatorialPlane.rotation.x = -Math.PI / 2;
+    this.scene.add(equatorialPlane);
+
+    this.axesHelper = new THREE.AxesHelper();
+    this.axesHelper.material.depthTest = false;
     this.scene.add(this.axesHelper);
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
